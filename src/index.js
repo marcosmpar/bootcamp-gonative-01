87415c468fff38f7f7b448ec4453637f61e50c97
Marcos Rodrigues <marcosmpar@gmail.com>
2019-05-08 19:00:59 -0400

    Alteração Todo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Todo title="Fazer Café" />
-        <Todo />
+     
       </View>
     )
   }
